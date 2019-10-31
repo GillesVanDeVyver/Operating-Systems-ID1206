308d0119f179c59c33e9a670bfa49e03050e457b
Emil Ståhl <emil.stahl@icloud.com>
2019-10-31 12:12:24 +0100

    1.2 done

--- Assignments/1. A heap, a stack, a bottle and a rack/1.2/code.c
+++ Assignments/1. A heap, a stack, a bottle and a rack/1.2/code.c
@@ -1,16 +1,24 @@
-
-#include <stdlib.h> 
-#include <stdio.h> 
-#include <sys/types.h> 
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
 #include <unistd.h>
 
-char global[] = "This is a global string";
+char global [] = "This is a global string";
+const int read_only = 123456;
+
 
 int main() {
-    int pid = getpid();
-    foo:
-    printf("Process identifier: %d\n", pid);
-    printf("Global string: %p\n", &global);
-    printf("The code: %p\n", &&foo);
+  int pid = getpid();
+  foo:
+    printf("process id: %d\n", pid);
+    printf("global string: %p\n", &global);
+    printf("the code: %p\n", &&foo);
+    printf("read only: %p\n", &read_only);
+
+    printf("\n\n /proc/%d/maps \n\n", pid);
+    char command[50];
+    sprintf(command, "cat /proc/%d/maps", pid);
+    system(command);
+
     return 0;
 }
