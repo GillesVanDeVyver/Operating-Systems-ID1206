c034ecaf007a1bfb4512f72af4d363045af4eb95
emilstahl97 <emil.stahl@icloud.com>
2019-11-03 15:36:51 +0100

    Exercise 2.4 done, version 2 with adress printing

--- Assignments/2. Hello Dolly/2.4/dolly.c
+++ Assignments/2. Hello Dolly/2.4/dolly.c
@@ -8,15 +8,15 @@ int main() {
     int x = 123, pid = fork();
 
     if (pid == 0) {
-        printf("Child : x is %d \n", x);
+        printf("Child : x is %d and the adress is %p \n", x, &x);
         x = 42; 
         sleep(1);
-        printf("Child x is %d \n", x);   
+        printf("Child x is %d and the adress is %p \n", x, &x);   
     } else {
-        printf("Mother: x is %d\n", x);
+        printf("Mother: x is %d and the adress is %p \n", x, &x);
         x = 13;
         sleep(1);
-        printf("Mother: x is %d\n", x);
+        printf("Mother: x is %d and the adress is %p \n", x, &x);
         wait(NULL);
     }
     return 0;
