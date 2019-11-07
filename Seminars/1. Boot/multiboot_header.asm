96e613539bf1f965a1ccf06b78dd088688a2657b
Emil Ståhl <emil.stahl@icloud.com>
2019-11-07 15:09:40 +0100

    created multiboot_header.asm

---
+++ Seminars/1. Boot/multiboot_header.asm
@@ -0,0 +1,14 @@
+section .multiboot_hader
+
+magic equ 0xe85250d6      ; multiboot 2
+arch equ 0                ; protected mode i386
+
+header_start:
+  dd magic                              ; magic number
+  dd arch                               ; architecture
+  dd header_end - header_start          ; header length
+  dd 0x100000000 - (magic + arch + (header_end - header_start))
+  dw 0  ; type
+  dw 0  ; flags
+  dd 8  ; size
+header_end:
