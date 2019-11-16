d77c19f892bf3a795909c0b1f016ac64d6db23ae
emilstahl97 <emil.stahl@icloud.com>
2019-11-16 17:39:43 +0100

    done

--- Assignments/5. My Malloc/mhysa.c
+++ Assignments/5. My Malloc/mhysa.c
@@ -1,18 +1,51 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+struct chunk {
+  int size;
+  struct chunk *next;
+};
+
+struct chunk* flist = NULL;
+
+void free(void *memory){
+  if(memory != NULL){
+    /* we're jumping back one chunk position */
+    struct chunk *cnk = (struct chunk*)((struct chunk*)memory - 1);
+    cnk->next = flist;
+    flist = cnk;
+  }
+  return;
+}
+
 void *malloc(size_t size){
   if(size == 0){
     return NULL;
   }
-  void *memory = sbrk(size);
-  if(memory == (void *) -1){
+  struct chunk *next = flist;
+  struct chunk *prev = NULL;
+
+  while(next != NULL){
+    if(next->size >= size){
+      if(prev != NULL){
+        prev->next = next->next;
+      } else {
+        flist = next->next;
+      }
+      return (void*)(next + 1);
+    } else {
+      prev = next;
+      next = next->next;
+    }
+  }
+
+  /* use sbrk to allocate new memory */
+  void *memory = sbrk(size + sizeof(struct chunk));
+  if(memory == (void *) - 1){
     return NULL;
   } else {
-    return memory;
+    struct chunk *cnk = (struct chunk*) memory;
+    cnk->size = size;
+    return (void*)(cnk + 1);
   }
-}
-
-void free(void *memory){
-  return;
 }
