8ac0483add7c0fad5feb7ed414a8f768a975da12
Emil Ståhl <emil.stahl@icloud.com>
2019-11-20 16:47:40 +0100

    Create ptmall.h

---
+++ Seminars/2. Memory Management/ptmall.h
@@ -0,0 +1,7 @@
+void *palloc(size_t request);
+void pree(void *memory);
+void init();
+void terminate();
+void sanity();
+void printCountLengthOfFlist(int bufferSize);
+void printSizeDistributionOfFlist(int bufferSize);
