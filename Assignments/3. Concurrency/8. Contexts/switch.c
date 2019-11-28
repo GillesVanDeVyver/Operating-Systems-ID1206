c874f716303ce80d9556c1a2db78ed75d5f9a729
Emil Ståhl <emil.stahl@icloud.com>
2019-11-28 17:27:14 +0100

    created switch.c

---
+++ Assignments/3. Concurrency/8. Contexts/switch.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ucontext.h>
+
+int main () {
+
+    int done = 0;
+    ucontext_t one;
+    ucontext_t two;
+
+    getcontext(&one);
+
+    printf("Hello \n");
+
+    if(!done) {
+        done=1;
+        swapcontext(&two, &one);
+    }
+    return 0;
+}
