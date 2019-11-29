62ee125f127aab02d3a32e2b72907d1e9ba1463b
Emil Ståhl <emil.stahl@icloud.com>
2019-11-29 15:04:55 +0100

    added iso

--- Seminars/1. Boot/Seminar1/kernel.asm
+++
@@ -1,8 +0,0 @@
-bits 32
-global start
-
-section .text
-
-start:
-  mov dword [0xb8000], 0x2f4b2f4f ; print 'OK' to screen
-  hlt
--- Seminars/1. Boot/Seminar1/myos/make.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-nasm -f elf32 kernel.asm
-ld -m elf_i386 -o kernel.bin -T linker.ld multiboot_header.o kernel.o myos.o
-gcc -m32 -c myos.c
-mv kernel.bin ./cdrom/boot
-mv myos.o ./cdrom/boot
-grub-mkrescue -o cdrom.iso cdrom
-qemu-system-x86_64 -cdrom cdrom.iso
