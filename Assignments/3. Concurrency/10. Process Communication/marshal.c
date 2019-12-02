75204d62eb005decf116cfbc67d07df9186a76b8
Emil Ståhl <emil.stahl@icloud.com>
2019-12-02 17:58:52 +0100

    Update marshal.c

--- Assignments/3. Concurrency/10. Process Communication/marshal.c
+++ Assignments/3. Concurrency/10. Process Communication/marshal.c
@@ -1,3 +1,4 @@
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -5,34 +6,32 @@
 #include <sys/wait.h>
 
 #define ITERATIONS 10
-#define BURSTS 10000000
+#define BURST 10
 
-int main () {
+int main() {
     int descr[2];
 
     assert(0 == pipe(descr));
-    
-    if(fork() == 0) {
+    if(fork() == 0) {    
         //consumer
-        for(int i = 0; i < ITERATIONS; i++) {
+        for(int i = 0; i < ITERATIONS; i++){
             double buffer;
             read(descr[0], &buffer, sizeof(double));
-            printf("Received %f\n", buffer);
+            printf("received %f\n", buffer);
             sleep(1);
         }
         printf("consumer done\n");
         return 0;
-    }
 
+    }
     //producer
-    for(int i = 0; i < ITERATIONS; i++) {
+    for(int i  = 0; i < ITERATIONS; i++) {
         double pi = 3.14*i;
         write(descr[1], &pi, sizeof(double));
     }
-    printf("producer burst %d done\n",i);
-    }   
+    printf("producer done\n");
+
     wait(NULL);
     printf("all done\n");
+
 }
-
-
