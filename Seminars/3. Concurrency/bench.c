7916dece18f092dbe8c1a43e735cc5486ecf44c6
Emil Ståhl <emil.stahl@icloud.com>
2019-12-06 19:03:02 +0100

    works

--- Seminars/3. Concurrency/bench.c
+++ Seminars/3. Concurrency/bench.c
@@ -3,159 +3,206 @@
 #include <pthread.h>
 #include "green.h"
 
+
+int flag = 0;
 green_cond_t cond;
 green_mutex_t mutex;
 pthread_cond_t emptyP, fullP;
 pthread_mutex_t mutexP;
 int numThreads = 2;
 
-int buffer, productions;
-green_cond_t empty, full;
-void produce()
-{
-    for (int i = 0; i < productions / (numThreads / 2); i++)
-    {
-        green_mutex_lock(&mutex);
-        while (buffer == 1) //wait for consumer before producing more
-            green_cond_wait(&empty, &mutex);
-        buffer = 1;
-        //printf("Produced!\n");
-        green_cond_signal(&full);
-        green_mutex_unlock(&mutex);
+int counter = 0;
+
+void* testCV(void* arg) {
+  int id = *(int*)arg;
+  int loop = 4;
+  while(loop > 0) {
+    if(flag == id) {
+      printf("thread %d: %d\n", id, loop);
+      for(int i = 0; i < 1000000; i++) {
+        if(i < 1)
+          i++;
+      }
+      loop--;
+      flag = (id + 1) % numThreads;
+      green_cond_signal(&cond);
+    } else {
+      //printf("Thread waiting: %d, flag: %d\n", id, flag);
+      green_cond_signal(&cond);
+      green_cond_wait(&cond, NULL);
     }
+  }
 }
 
-void consume()
-{
-
-    for (int i = 0; i < productions / (numThreads / 2); i++)
-    {
-        green_mutex_lock(&mutex);
-        while (buffer == 0) //wait for producing before consuming
-            green_cond_wait(&empty, &mutex);
-        buffer = 0;
-        //printf("Consumed!\n");
-        green_cond_signal(&empty);
-        green_mutex_unlock(&mutex);
-    }
+void* test(void* arg) {
+  int id = *(int*)arg;
+  int loop = 100000;
+  while(loop > 0) {
+    //if(loop % 1000 == 0)
+      //printf("thread %d: %d\n", id, loop/1000);
+    loop--;
+    //green_yield();
+  }
 }
 
-void produceP()
-{
-
-    for (int i = 0; i < productions / (numThreads / 2); i++)
-    {
-        pthread_mutex_lock(&mutexP);
-        while (buffer == 1) //wait for consumer before producing more
-            pthread_cond_wait(&emptyP, &mutexP);
-        buffer = 1;
-        //printf("Produced!\n");
-        pthread_cond_signal(&fullP);
-        pthread_mutex_unlock(&mutexP);
-    }
+void* testJoin(void* arg) {
+  green_t* joined = (green_t*)arg;
+  printf("joining...\n");
+  green_join(joined);
+  printf("joined!\n");
+  for(int i = 0; i < 10000; i++)
+    if(i > 8)
+      i++;
 }
 
-void consumeP()
-{
+void* testSharedResource(void* arg) {
+  int id = *(int*)arg;
+  for(int i = 0; i < 1000000; i++) {
+    green_mutex_lock(&mutex);
+    int temp = counter;
 
-    for (int i = 0; i < productions / (numThreads / 2); i++)
-    {
-        pthread_mutex_lock(&mutexP);
-        while (buffer == 0) //wait for producer before consuming
-            pthread_cond_wait(&emptyP, &mutexP);
-        buffer = 0;
-        //printf("Consumed!\n");
-        pthread_cond_signal(&emptyP);
-        pthread_mutex_unlock(&mutexP);
-    }
+    int dummy = 0;
+    if(temp > 1337) // waste time between read and write
+      dummy++;
+
+    temp++;
+    counter = temp;
+    green_mutex_unlock(&mutex);
+  }
 }
 
-void *testConsumerProducer(void *arg)
-{
-
-    int id = *(int *)arg;
-    if (id % 2 == 0)
-    {
-        produce();
-    }
-    else
-    {
-        consume();
-    }
+int buffer;
+int productions;
+green_cond_t full, empty;
+void produce() {
+  for(int i = 0; i < productions/(numThreads/2); i++) {
+    green_mutex_lock(&mutex);
+    while(buffer == 1) // wait for consumer before producing more
+      green_cond_wait(&empty, &mutex);
+    buffer = 1;
+    //printf("Produced!\n");
+    green_cond_signal(&full);
+    green_mutex_unlock(&mutex);
+  }
 }
 
-void *testConsumerProducerP(void *arg)
-{
-
-    int id = *(int *)arg;
-    if (id % 2 == 0)
-    {
-        produceP();
-    }
-    else
-    {
-        consumeP();
-    }
+void consume() {
+  for(int i = 0; i < productions/(numThreads/2); i++) {
+    green_mutex_lock(&mutex);
+    while(buffer == 0) // wait for producer before consuming
+      green_cond_wait(&full, &mutex);
+    buffer = 0;
+    //printf("Consumed!\n");
+    green_cond_signal(&empty);
+    green_mutex_unlock(&mutex);
+  }
 }
 
-void testGreen(int *args)
-{
-    green_t threads[numThreads];
-
-    for (int i = 0; i < numThreads; i++)
-        green_create(&threads[i], testConsumerProducer, &args[i]);
-
-    for (int i = 0; i < numThreads; i++)
-        green_join(&threads[i]);
+void produceP() { // heahae
+  for(int i = 0; i < productions/(numThreads/2); i++) {
+    pthread_mutex_lock(&mutexP);
+    while(buffer == 1) // wait for consumer before producing more
+      pthread_cond_wait(&emptyP, &mutexP);
+    buffer = 1;
+    //printf("Produced!\n");
+    pthread_cond_signal(&fullP);
+    pthread_mutex_unlock(&mutexP);
+  }
 }
 
-void testPthread(int *args)
-{
+void consumeP() { // heahae
+  for(int i = 0; i < productions/(numThreads/2); i++) {
+    pthread_mutex_lock(&mutexP);
+    while(buffer == 0) // wait for producer before consuming
+      pthread_cond_wait(&fullP, &mutexP);
+    buffer = 0;
+    //printf("Consumed!\n");
+    pthread_cond_signal(&emptyP);
+    pthread_mutex_unlock(&mutexP);
+  }
+}
 
-    pthread_t threads[numThreads];
+void* testConsumerProducer(void* arg) {
+  int id = *(int*)arg;
+  if(id % 2 == 0) { // producer
+    //printf("Producing...\n");
+    produce();
+  } else { // consumer
+    //printf("Consuming...\n");
+    consume();
+  }
+}
 
-    for (int i = 0; i < numThreads; i++)
-        pthread_create(&threads[i], NULL, testConsumerProducerP, &args[i]);
+void* testConsumerProducerP(void* arg) {
+  int id = *(int*)arg;
+  if(id % 2 == 0) { // producer
+    //printf("Producing...\n");
+    produceP();
+  } else { // consumer
+    //printf("Consuming...\n");
+    consumeP();
+  }
+}
 
-    for (int i = 0; i < numThreads; i++)
-        pthread_join(threads[i], NULL);
+void testGreen(int* args) {
+  green_t threads[numThreads];
+
+  for(int i = 0; i < numThreads; i++)
+    green_create(&threads[i], testConsumerProducer, &args[i]);
+  /*
+  green_create(&threads[0], test, &args[0]);
+  for(int i = 1; i < numThreads; i++)
+    green_create(&threads[i], testJoin, &threads[0]);
+  */
+  for(int i = 0; i < numThreads; i++)
+    green_join(&threads[i]);
+}
+
+void testPthread(int* args) {
+  pthread_t threads[numThreads];
+
+  for(int i = 0; i < numThreads; i++)
+    pthread_create(&threads[i], NULL, testConsumerProducerP, &args[i]);
+
+  for(int i = 0; i < numThreads; i++)
+    pthread_join(threads[i], NULL);
 }
 
 int main() {
+  clock_t c_start, c_stop;
+  double processTimeGreen = 0, processTimeP = 0;
+  green_cond_init(&cond);
+  green_cond_init(&full);
+  green_cond_init(&empty);
+  green_mutex_init(&mutex);
 
-    clock_t c_start, c_stop;
-    double processTimeGreen = 0, processTimeP = 0;
-    green_cond_init(&cond);
-    green_cond_init(&empty);
-    green_cond_init(&full);
-    green_mutex_init(&mutex);
+  pthread_cond_init(&fullP, NULL);
+  pthread_cond_init(&emptyP, NULL);
+  pthread_mutex_init(&mutexP, NULL);
+  printf("#Benchmark, creating and producing/consuming with threads!\n#\n#\n");
+  printf("#{#productions\ttimeGreen(ms)\ttimePthread(ms)}\n");
+  int numRuns = 10;
+  for(int run = 1; run <= numRuns; run++) {
+    //printf("Starting run %d\n", run);
+    buffer = 0;
+    productions = 1000 * 2 * run; // Must be multiple of 2
 
-    pthread_cond_init(&fullP, NULL);
-    pthread_cond_init(&emptyP, NULL);
-    pthread_mutex_init(&mutexP, NULL);
+    int args[numThreads];
+    for(int i = 0; i < numThreads; i++)
+      args[i] = i;
 
-    printf("#Benchmark, creating and producing/consuming with threads!\n#\n#\n");
-    printf("#{#productions\ttimeGreen(ms)\ttimePthread(ms)}\n");
+    c_start = clock();
+    testGreen(args);
+    c_stop = clock();
+    processTimeGreen = ((double)(c_stop - c_start)) / ((double)CLOCKS_PER_SEC/1000);
+    //printf("buffer: %d\n", buffer);
+    c_start = clock();
+    testPthread(args);
+    c_stop = clock();
+    processTimeP = ((double)(c_stop - c_start)) / ((double)CLOCKS_PER_SEC/1000);
+    printf("%d\t%f\t%f\n", productions, processTimeGreen, processTimeP);
+  }
+  // printf("done\n");
 
-    int numRuns = 10;
-    for(int run = 1; run <= numRuns; run++) {
-        buffer = 0;
-        productions = 1000 * 2 * run;
-
-        int args [numThreads];
-        for(int i = 0; i < numThreads; i++)
-            args[i] = i;
-
-        c_start = clock();
-        testGreen(args);
-        c_stop = clock();
-        processTimeGreen = ((double)(c_stop-c_start)) / ((double)CLOCKS_PER_SEC/1000);
-        c_start = clock();
-        testPthread(args);
-        c_stop = clock();
-        processTimeP = ((double)(c_stop-c_start)) / ((double)CLOCKS_PER_SEC/1000);
-        printf("%d\t%f\t%f\n", productions, processTimeGreen, processTimeP);    
-    }    
-
-    printf("done\n");
+  return 0;
 }
