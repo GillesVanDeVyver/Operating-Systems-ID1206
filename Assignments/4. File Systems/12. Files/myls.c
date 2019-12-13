90c4de7da3fb087f9c3da3f5ddacb18a35fa5f10
Emil Ståhl <emil.stahl@icloud.com>
2019-12-13 16:34:09 +0100

    works

--- Assignments/4. File Systems/12. Files/myls.c
+++ Assignments/4. File Systems/12. Files/myls.c
@@ -1,56 +1,65 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/types.h>
 #include <dirent.h>
+#include <assert.h>
 #include <sys/stat.h>
-
+#include <unistd.h>
+#include <errno.h>
 
 int main(int argc, char *argv[]) {
-struct stat file_st;
 
-    if(argc < 2) {
-        perror("Usage: myls <dir>\n");
-        return -1;
+  if( argc < 2 ) {
+    perror("usage: myls <dir>\n");
+    return -1;
+  }
+
+  char *path = argv[1];
+
+  struct stat file_st;
+  stat(path, &file_st);  
+
+  printf("The directory %s with inode %8lu\n", path,  file_st.st_ino);
+  
+  DIR *dirp = opendir(path);
+
+  struct dirent *entry;
+
+  errno = 0;
+  
+  while((entry = readdir(dirp)) != NULL) {
+    switch( entry->d_type) {
+    case DT_BLK  :  // This is a block device. 
+      printf("b:");
+      break;      
+    case DT_CHR  :  //This is a character device.
+      printf("c:");
+      break;      
+    case DT_DIR  :  //This is a directory.
+        printf("d:");
+        break;
+    case DT_FIFO :  //This is a named pipe (FIFO).
+      printf("p:");
+      break;
+    case DT_LNK  :  //This is a symbolic link.
+      printf("l:");
+      break;
+    case DT_REG  :  //This is a regular file.
+      printf("f:");
+      break;
+    case DT_SOCK :  //This is a UNIX domain socket.
+      printf("s:");
+      break;
+    case DT_UNKNOWN :  // The file type is unknown.
+      printf("u:");
+      break;
     }
 
-    char *path = argv[1];
+    printf("\tinode: %8lu",  entry->d_ino);
+    printf("\tname: %s\n", entry->d_name);      
 
-    DIR *dirp = opendir(path);
-
-    struct dirent *entry;
-
-    while ((entry = readdir(dirp)) != NULL) {
-        switch( entry->d_type) {
-            case DT_BLK : // This is a block device
-                printf("b:");
-                break;
-            case DT_CHR : //This is a character device
-                printf("c:");
-                break;
-            case DT_DIR :
-                printf("d:"); //This is a directory
-                break;
-            case DT_FIFO : // This is a named pipe
-                printf("p:");
-                break;
-            case DT_LNK : // This is a symbolic link
-                printf("l:");
-                break;
-            case DT_REG : // This is a regular file
-                printf("f:");
-                break;
-            case DT_SOCK : // This is a UNIX domain socket
-                printf("s:");
-                break;
-            case DT_UNKNOWN : //The file is unknown
-                printf("u:");
-                break;
-        }
-        
-        fstatat(dirfd(dirp), entry->d_name, &file_st, 0);
-
-        printf("\tinode %lu", entry->d_ino);
-        printf("\tdev: 0x%lx", file_st.st_dev);
-        printf("\tsize: %lu", file_st.st_size);
-        printf("\tname: %s\n", entry->d_name);
-    }
+  }    
+    
+  if(errno != 0) {
+    perror("something happened");
+  }
 }
